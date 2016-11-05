1cc4dfb628461de76f59a1c9d3092b6ecd2e0559
Fenixsar <fenixsar@yandex.ru>
2016-11-05 11:42:50 +0300

    123

--- server/boot/express.js
+++ server/boot/express.js
@@ -1,15 +1,12 @@
 //boot/express.js
 var express = require('express'),
 	path = require('path'),
-	cookieParser = require('cookie-parser'),
-	logger = require('morgan');
+	cookieParser = require('cookie-parser');
 
 module.exports = function(app) {
 	app.set('views', path.join(__dirname + "/../..", 'templates'));
-	app.set('view engine', 'jade');
+	app.set('view engine', 'pug');
 
 	app.use(express.static(path.join(__dirname + "/../..", 'public')));
 	app.use(cookieParser());
-
-	app.use(logger('combined'));
 };
